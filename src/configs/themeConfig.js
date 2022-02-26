94ab4d4acb44e3dd1ce1545204331f0f6dcb2038
Phung Tuan <mr.bichkhe@gmail.com>
2022-02-27 01:03:21 +0700

    theme dark +  navbar sticky

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -8,7 +8,7 @@ const themeConfig = {
   },
   layout: {
     isRTL: false,
-    skin: 'light', // light, dark, bordered, semi-dark
+    skin: 'dark', // light, dark, bordered, semi-dark
     routerTransition: 'fadeIn', // fadeIn, fadeInLeft, zoomIn, none or check this for more transition https://animate.style/
     type: 'vertical', // vertical, horizontal
     contentWidth: 'boxed', // full, boxed
@@ -18,7 +18,7 @@ const themeConfig = {
     },
     navbar: {
       // ? For horizontal menu, navbar type will work for navMenu type
-      type: 'floating', // static , sticky , floating, hidden
+      type: 'sticky', // static , sticky , floating, hidden
       backgroundColor: 'white' // BS color options [primary, success, etc]
     },
     footer: {
