dfdfc84a9b870b4232e3b61df30568a6f52c4aff
Phung Tuan <mr.bichkhe@gmail.com>
2022-02-27 12:21:53 +0700

    [Features] Logout

--- src/@core/layouts/components/navbar/UserDropdown.js
+++ src/@core/layouts/components/navbar/UserDropdown.js
@@ -1,6 +1,6 @@
 // ** React Imports
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 // ** Custom Components
 import Avatar from '@components/avatar'
@@ -9,74 +9,80 @@ import Avatar from '@components/avatar'
 // import { isUserLoggedIn } from '@utils'
 
 // ** Third Party Components
-import { User, Mail, CheckSquare, MessageSquare, Settings, CreditCard, HelpCircle, Power } from 'react-feather'
+import {User, Mail, CheckSquare, MessageSquare, Settings, CreditCard, HelpCircle, Power} from 'react-feather'
 
 // ** Reactstrap Imports
-import { UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem } from 'reactstrap'
+import {UncontrolledDropdown, DropdownMenu, DropdownToggle, DropdownItem} from 'reactstrap'
 
 // ** Default Avatar Image
 import defaultAvatar from '@src/assets/images/portrait/small/avatar-s-11.jpg'
+import {handleLogout} from "../../../../redux/authentication"
+import {useDispatch} from "react-redux"
 
 const UserDropdown = () => {
-  // ** State
-  const [userData] = useState(null)
+    // ** State
+    const [userData] = useState(null)
+    const dispatch = useDispatch()
+    //** ComponentDidMount
+    // useEffect(() => {
+    //   if (isUserLoggedIn() !== null) {
+    //     setUserData(JSON.parse(localStorage.getItem('userData')))
+    //   }
+    // }, [])
 
-  //** ComponentDidMount
-  // useEffect(() => {
-  //   if (isUserLoggedIn() !== null) {
-  //     setUserData(JSON.parse(localStorage.getItem('userData')))
-  //   }
-  // }, [])
+    const onLogOut = () => {
+        dispatch(handleLogout())
+    }
 
-  //** Vars
-  const userAvatar = (userData && userData.avatar) || defaultAvatar
+    //** Vars
+    const userAvatar = (userData && userData.avatar) || defaultAvatar
 
-  return (
-    <UncontrolledDropdown tag='li' className='dropdown-user nav-item'>
-      <DropdownToggle href='/' tag='a' className='nav-link dropdown-user-link' onClick={e => e.preventDefault()}>
-        <div className='user-nav d-sm-flex d-none'>
-          <span className='user-name fw-bold'>{(userData && userData['username']) || 'John Doe'}</span>
-          <span className='user-status'>{(userData && userData.role) || 'Admin'}</span>
-        </div>
-        <Avatar img={userAvatar} imgHeight='40' imgWidth='40' status='online' />
-      </DropdownToggle>
-      <DropdownMenu end>
-        <DropdownItem tag='a' href='/pages/profile' onClick={e => e.preventDefault()}>
-          <User size={14} className='me-75' />
-          <span className='align-middle'>Profile</span>
-        </DropdownItem>
-        <DropdownItem tag='a' href='/apps/email' onClick={e => e.preventDefault()}>
-          <Mail size={14} className='me-75' />
-          <span className='align-middle'>Inbox</span>
-        </DropdownItem>
-        <DropdownItem tag='a' href='/apps/todo' onClick={e => e.preventDefault()}>
-          <CheckSquare size={14} className='me-75' />
-          <span className='align-middle'>Tasks</span>
-        </DropdownItem>
-        <DropdownItem tag='a' href='/apps/chat' onClick={e => e.preventDefault()}>
-          <MessageSquare size={14} className='me-75' />
-          <span className='align-middle'>Chats</span>
-        </DropdownItem>
-        <DropdownItem divider />
-        <DropdownItem tag='a' href='/pages/account-settings' onClick={e => e.preventDefault()}>
-          <Settings size={14} className='me-75' />
-          <span className='align-middle'>Settings</span>
-        </DropdownItem>
-        <DropdownItem tag='a' href='/pages/pricing' onClick={e => e.preventDefault()}>
-          <CreditCard size={14} className='me-75' />
-          <span className='align-middle'>Pricing</span>
-        </DropdownItem>
-        <DropdownItem tag='a' href='/pages/faq' onClick={e => e.preventDefault()}>
-          <HelpCircle size={14} className='me-75' />
-          <span className='align-middle'>FAQ</span>
-        </DropdownItem>
-        <DropdownItem tag={Link} to='/login'>
-          <Power size={14} className='me-75' />
-          <span className='align-middle'>Logout</span>
-        </DropdownItem>
-      </DropdownMenu>
-    </UncontrolledDropdown>
-  )
+    return (
+        <UncontrolledDropdown tag='li' className='dropdown-user nav-item'>
+            <DropdownToggle href='/' tag='a' className='nav-link dropdown-user-link' onClick={e => e.preventDefault()}>
+                <div className='user-nav d-sm-flex d-none'>
+                    <span className='user-name fw-bold'>{(userData && userData['username']) || 'John Doe'}</span>
+                    <span className='user-status'>{(userData && userData.role) || 'Admin'}</span>
+                </div>
+                <Avatar img={userAvatar} imgHeight='40' imgWidth='40' status='online'/>
+            </DropdownToggle>
+            <DropdownMenu end>
+                <DropdownItem tag='a' href='/pages/profile' onClick={e => e.preventDefault()}>
+                    <User size={14} className='me-75'/>
+                    <span className='align-middle'>Profile</span>
+                </DropdownItem>
+                <DropdownItem tag='a' href='/apps/email' onClick={e => e.preventDefault()}>
+                    <Mail size={14} className='me-75'/>
+                    <span className='align-middle'>Inbox</span>
+                </DropdownItem>
+                <DropdownItem tag='a' href='/apps/todo' onClick={e => e.preventDefault()}>
+                    <CheckSquare size={14} className='me-75'/>
+                    <span className='align-middle'>Tasks</span>
+                </DropdownItem>
+                <DropdownItem tag='a' href='/apps/chat' onClick={e => e.preventDefault()}>
+                    <MessageSquare size={14} className='me-75'/>
+                    <span className='align-middle'>Chats</span>
+                </DropdownItem>
+                <DropdownItem divider/>
+                <DropdownItem tag='a' href='/pages/account-settings' onClick={e => e.preventDefault()}>
+                    <Settings size={14} className='me-75'/>
+                    <span className='align-middle'>Settings</span>
+                </DropdownItem>
+                <DropdownItem tag='a' href='/pages/pricing' onClick={e => e.preventDefault()}>
+                    <CreditCard size={14} className='me-75'/>
+                    <span className='align-middle'>Pricing</span>
+                </DropdownItem>
+                <DropdownItem tag='a' href='/pages/faq' onClick={e => e.preventDefault()}>
+                    <HelpCircle size={14} className='me-75'/>
+                    <span className='align-middle'>FAQ</span>
+                </DropdownItem>
+                <DropdownItem tag={Link} to='/login'>
+                    <Power size={14} className='me-75'/>
+                    <span className='align-middle' onClick={onLogOut}>Logout</span>
+                </DropdownItem>
+            </DropdownMenu>
+        </UncontrolledDropdown>
+    )
 }
 
 export default UserDropdown
--- src/redux/authentication.js
+++ src/redux/authentication.js
@@ -1,5 +1,5 @@
 // ** Redux Imports
-import { createSlice } from '@reduxjs/toolkit'
+import {createSlice} from '@reduxjs/toolkit'
 
 // ** UseJWT import to get config
 import useJwt from '@src/auth/jwt/useJwt'
@@ -8,51 +8,51 @@ import {getData} from "./testForm"
 const config = useJwt.jwtConfig
 
 const initialUser = () => {
-  const item = window.localStorage.getItem('userData')
-  //** Parse stored json or if none return initialValue
-  return item ? JSON.parse(item) : {}
+    const item = window.localStorage.getItem('userData')
+    //** Parse stored json or if none return initialValue
+    return item ? JSON.parse(item) : {}
 }
 
 export const authSlice = createSlice({
-  name: 'authentication',
-  initialState: {
-    userData: initialUser()
-  },
-  reducers: {
-    handleLogin: (state, action) => {
-      console.log('state:', state, action.payload)
-      // state.userData = action.payload.userData
-      state[config.storageTokenKeyName] = action.payload[config.storageTokenKeyName]
-      state[config.storageRefreshTokenKeyName] = action.payload[config.storageRefreshTokenKeyName]
-      // localStorage.setItem('userData', JSON.stringify(action.payload.userData))
-      localStorage.setItem(config.storageTokenKeyName, action.payload.accessToken)
-      localStorage.setItem(config.storageRefreshTokenKeyName, action.payload.refreshToken)
+    name: 'authentication',
+    initialState: {
+        userData: initialUser()
     },
-    handleMe: (state, action) => {
-      state.userData = action.payload.userData
-      localStorage.setItem('userData', JSON.stringify(action.payload))
+    reducers: {
+        handleLogin: (state, action) => {
+            console.log('state:', state, action.payload)
+            // state.userData = action.payload.userData
+            state[config.storageTokenKeyName] = action.payload[config.storageTokenKeyName]
+            state[config.storageRefreshTokenKeyName] = action.payload[config.storageRefreshTokenKeyName]
+            // localStorage.setItem('userData', JSON.stringify(action.payload.userData))
+            localStorage.setItem(config.storageTokenKeyName, action.payload.accessToken)
+            localStorage.setItem(config.storageRefreshTokenKeyName, action.payload.refreshToken)
+        },
+        handleMe: (state, action) => {
+            state.userData = action.payload.userData
+            localStorage.setItem('userData', JSON.stringify(action.payload))
+        },
+        handleLogout: state => {
+            state.userData = {}
+            state[config.storageTokenKeyName] = null
+            state[config.storageRefreshTokenKeyName] = null
+            // ** Remove user, accessToken & refreshToken from localStorage
+            localStorage.removeItem('userData')
+            localStorage.removeItem(config.storageTokenKeyName)
+            localStorage.removeItem(config.storageRefreshTokenKeyName)
+        }
     },
-    handleLogout: state => {
-      state.userData = {}
-      state[config.storageTokenKeyName] = null
-      state[config.storageRefreshTokenKeyName] = null
-      // ** Remove user, accessToken & refreshToken from localStorage
-      localStorage.removeItem('userData')
-      localStorage.removeItem(config.storageTokenKeyName)
-      localStorage.removeItem(config.storageRefreshTokenKeyName)
+    extraReducers: builder => {
+        console.log('authentication:extraReducers')
+        builder.addCase(getData.fulfilled, (state, action) => {
+            state.data = action.payload.data
+            state.params = action.payload.params
+            state.allData = action.payload.allData
+            state.total = action.payload.totalPages
+        })
     }
-  },
-  extraReducers: builder => {
-    console.log('authentication:extraReducers')
-    builder.addCase(getData.fulfilled, (state, action) => {
-      state.data = action.payload.data
-      state.params = action.payload.params
-      state.allData = action.payload.allData
-      state.total = action.payload.totalPages
-    })
-  }
 })
 
-export const { handleLogin, handleLogout, handleMe } = authSlice.actions
+export const {handleLogin, handleLogout, handleMe} = authSlice.actions
 
 export default authSlice.reducer
